c24e0b12c4eb1b39d6df743d54027a90d6171827
Calvinan132 <trinhhoaian2303@gmail.com>
2026-05-19 20:34:47 +0700

    feat: implement API fetching for raw uploads and update BatchTable component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/appwrite/:path*",
+        destination: "https://cloud.appwrite.io/v1/:path*",
+      },
+      {
+        source: "/api/appwrite-func",
+        destination: "https://69c7e0fb00237ca9bdcc.syd.appwrite.run/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
