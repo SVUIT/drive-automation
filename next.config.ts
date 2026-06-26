cfd28a7001f6bd7d16e5e20c048fef655f8f2388
Calvinan132 <trinhhoaian2303@gmail.com>
2026-06-26 23:37:25 +0700

    feat: update API rewrites and add Appwrite function proxy route

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    const appwriteFunctionUrl = process.env.APPWRITE_FUNCTION_URL;
     return [
       {
         source: "/api/appwrite/:path*",
         destination: "https://cloud.appwrite.io/v1/:path*",
       },
-      ...(appwriteFunctionUrl
-        ? [
-            {
-              source: "/api/appwrite-func/:path*",
-              destination: appwriteFunctionUrl,
-            },
-          ]
-        : []),
     ];
   },
 };
